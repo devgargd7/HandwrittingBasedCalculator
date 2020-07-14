0d65f2efcd09347b8bfccaba062695334a762b50
Dev Garg <devgargd7@gmail.com>
2020-07-14 10:51:56 +0530

    deploy as a Flask app

---
+++ app.py
@@ -0,0 +1,59 @@
+import flask
+from flask import Flask, render_template, url_for, request
+import base64
+import numpy as np
+import cv2
+from Predict import Predictit, Calculate
+import json
+
+# Initialize the useless part of the base64 encoded image.
+init_Base64 = 21
+
+# Initializing new Flask instance. Find the html template in "templates".
+app = flask.Flask(__name__, template_folder='templates')
+
+# First route : Render the initial drawing template
+
+
+@app.route('/')
+def home():
+    return render_template('draw.html')
+
+
+# Second route : Use our model to make prediction - render the results page.
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Preprocess the image : set the image to 28x28 shape
+        # Access the image
+        draw = request.form['url']
+        # Removing the useless part of the url.
+        draw = draw[init_Base64:]
+        # Decoding
+        draw_decoded = base64.b64decode(draw)
+        image = np.asarray(bytearray(draw_decoded), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+        predictions = Predictit(255-image)
+        print(predictions)
+        result, error = Calculate(predictions)
+        # Resizing and reshaping to keep the ratio.
+        # resized = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
+        # cv2.imwrite('image.jpeg', resized)
+        # vect = np.asarray(image, dtype="uint8")
+        # vect = vect.reshape(1, 28, 28, 1).astype('float32')
+        # vect = vect/255.0
+        # Launch prediction
+        # my_prediction = model.predict(vect)
+        # Getting the index of the maximum prediction
+        # index = np.argmax(my_prediction[0])
+        # Associating the index and its value within the dictionnary
+        if error == "":
+            return render_template('results.html', prediction=predictions, result=result)
+        else:
+            return render_template('results.html', prediction=predictions, error=error)
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
